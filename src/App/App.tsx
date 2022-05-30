422d2477f470dab55b111293b958c963fc453271
vinsag <vinsag@yahoo.com>
2022-05-30 11:04:54 +0100

    Reformulation principale  et vidage projet

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // Comment js
+  /* Comment mutltignes js */
+  return (
+    <div className="App">
+      {/* Comment dans XML */}
+      DEMAT BREIZH
+    </div>
+  );
+}
+
+export default App;
