4e1ade866199e8149a2edb6635f612394a514088
vinsag <vinsag@yahoo.com>
2022-05-30 14:55:09 +0100

    Module style

--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes  from 'prop-types';
+import style from './Button.module.css';
 
 
 function  Button(props) {
     console.log(props);
-    return <button type={props.type}>{props.children}</button>
+    return <button className={style.Button} 
+        type={props.type}
+    >{props.children}</button>
 }
 
 Button.propTypes = {
